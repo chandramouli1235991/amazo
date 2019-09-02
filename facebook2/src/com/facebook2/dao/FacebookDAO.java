b0381e549f7760795efd03bfe50f1dc2c7d49443
SC57554 <SC57554@TRNW56SLPC0062.ind.zensar.com>
2019-09-02 13:13:07 +0530

    gtt

---
+++ facebook2/src/com/facebook2/dao/FacebookDAO.java
@@ -0,0 +1,19 @@
+package com.facebook2.dao;
+
+import com.facebook2.entity.FacebookEmployee;
+
+public class FacebookDAO implements FacebookDAOInterface {
+private  FacebookDAO() {
+	
+}
+	public static FacebookDAOInterface createDaoObject() {
+		// TODO Auto-generated method stub
+		return new FacebookDAO();
+	}
+	@Override
+	public int createProfileDAO(FacebookEmployee fe) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
